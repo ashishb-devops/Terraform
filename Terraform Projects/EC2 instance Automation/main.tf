a534f9976225792ae4556de952e6999011c4cc53
ashishb-devops <ashish.bethi10@gmail.com>
2025-07-13 21:03:38 +0530

    Update main.tf

--- Terraform Projects/EC2 instance Automation/main.tf
+++ Terraform Projects/EC2 instance Automation/main.tf
@@ -1,1 +1,58 @@
+# Use the AWS provider
+provider "aws" {
+  region = var.aws_region
+}
 
+# Use the default VPC
+resource "aws_default_vpc" "default" {}
+
+# Create a security group to allow SSH (22) and HTTP (80)
+resource "aws_security_group" "web_sg" {
+  name        = "web-sg"
+  description = "Allow SSH and HTTP"
+  vpc_id      = aws_default_vpc.default.id
+
+  ingress {
+    description = "SSH Access"
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "HTTP Access"
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "web-sg"
+  }
+}
+
+# Launch an EC2 instance
+resource "aws_instance" "web" {
+  ami           = var.ami_id
+  instance_type = var.instance_type
+  key_name      = var.key_name
+  security_groups = [aws_security_group.web_sg.name]
+
+  tags = {
+    Name = "Terraform-EC2"
+  }
+
+  root_block_device {
+    volume_size = 10
+    volume_type = "gp2"
+  }
+}
