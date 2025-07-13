c15e59a57cab3c0639b5ee9ed34827c4498f8013
ashishb-devops <ashish.bethi10@gmail.com>
2025-07-13 21:05:07 +0530

    Create variables.tf

---
+++ Terraform Projects/EC2 instance Automation/variables.tf
@@ -0,0 +1,24 @@
+# AWS region
+variable "aws_region" {
+  description = "The AWS region to deploy resources in"
+  type        = string
+}
+
+# AMI ID
+variable "ami_id" {
+  description = "AMI ID for the EC2 instance"
+  type        = string
+}
+
+# Instance type
+variable "instance_type" {
+  description = "Type of EC2 instance"
+  type        = string
+  default     = "t2.micro"
+}
+
+# Key pair name
+variable "key_name" {
+  description = "Name of the SSH key pair"
+  type        = string
+}
